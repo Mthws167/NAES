3a8fcf32abbb68d0459055d15cab36c52eb6b479
Mthws167 <mthws.henrique@hotmail.com>
2023-03-06 22:44:46 -0300

    layout

--- BikeControl/paginas/urls.py
+++ BikeControl/paginas/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from views import IndexView
+from .views import IndexView
 
 urlpatterns = [
 
-    path("home/", NomeDaView.as_view(), name ="index"),
+    path("", IndexView.as_view(), name="index"),
 
 ]
