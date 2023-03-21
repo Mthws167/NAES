44d3ec41b25cfc07548ac6660bae4d43a4819ded
Mthws167 <mthws.henrique@hotmail.com>
2023-03-20 22:39:53 -0300

    Iniciando Classes

--- BikeControl/cadastros/admin.py
+++ BikeControl/cadastros/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Administrador, Empresa
 
 # Register your models here.
+
+admin.site.register(Administrador)
+admin.site.register(Empresa)
--- BikeControl/cadastros/models.py
+++ BikeControl/cadastros/models.py
@@ -1,25 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Empresa(models.Model):
-    nome = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, 
-                                unique=True)
-    telefone = models.CharField(max_length=15, 
-                                unique=True, 
-                                verbose_name="(XX)XXXXX-XXXX")
-    documento = models.CharField(max_length=18, 
-                                unique=True,
-                                help_text="Insira seu CNPJ", 
-                                verbose_name="XX.XXX.XXX/XXXX-XX")
-    senha = models.CharField(max_length=50)
-    dataCriacao = models.DateTimeField(auto_now_add=True)
-    dataAtualizacao = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.nome} ({self.documento})"
-
-
 class Administrador(models.Model):
     nome = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, 
@@ -33,9 +14,28 @@ class Administrador(models.Model):
                                  help_text="Insira seu CPF", 
                                  verbose_name="XXX.XXX.XXX-XX")
     senha = models.CharField(max_length=50)
-    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
     dataCriacao = models.DateTimeField(auto_now_add=True)
     dataAtualizacao = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.nome} ({self.telefone})"
+
+
+class Empresa(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50,
+                              unique=True)
+    telefone = models.CharField(max_length=15,
+                                unique=True,
+                                verbose_name="(XX)XXXXX-XXXX")
+    documento = models.CharField(max_length=18,
+                                 unique=True,
+                                 help_text="Insira seu CNPJ",
+                                 verbose_name="XX.XXX.XXX/XXXX-XX")
+    senha = models.CharField(max_length=50)
+    administrador = models.ForeignKey(Administrador, on_delete=models.PROTECT)
+    dataCriacao = models.DateTimeField(auto_now_add=True)
+    dataAtualizacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.nome} ({self.documento})"
