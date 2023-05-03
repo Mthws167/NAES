46b9d457edda185b1a47ff109530ff1f1bd3a230
Mthws167 <mthws.henrique@hotmail.com>
2023-05-03 09:31:14 -0300

    Página sobre

--- BikeControl/paginas/urls.py
+++ BikeControl/paginas/urls.py
@@ -4,5 +4,7 @@ from .views import IndexView
 urlpatterns = [
 
     path("", IndexView.as_view(), name="index"),
+    path("sobre/",views.LoginView.as_view(template_name="paginas/sobre.html",name="sobre"),
+
 
 ]
