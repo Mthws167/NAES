a980ecb59de120af03049c6834e2f9b28905d9d1
Mthws167 <mthws.henrique@hotmail.com>
2023-11-23 20:10:04 -0300

    cadastro

--- BikeControl/usuarios/urls.py
+++ BikeControl/usuarios/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views
+from .views import UsuarioCreate
 
 
 urlpatterns = [
     path("login/",views.LoginView.as_view(template_name="cadastros/form.html",extra_context={"titulo": "Autenticação"}),name="login"),
     path("logout/",views.LogoutView.as_view(),name="logout"),
+    path("registrar/", UsuarioCreate.as_view(),name='registrar'),
 ]
--- BikeControl/usuarios/views.py
+++ BikeControl/usuarios/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from .forms import UsuarioForm
+from django.urls import reverse_lazy
+
+class UsuarioCreate(CreateView):
+    template_name="cadastros/form.html"
+    form_class = UsuarioForm
+    success_url= reverse_lazy('login')
 
 # Create your views here.
+    def get_context_data(self, *args,**kwargs) :
+        context = super().get_context_data(*args, **kwargs)
+
+        context['titulo'] =' Registro novo Usuário'
+
+        return context
